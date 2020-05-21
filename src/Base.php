eb6595b64a80ee0f6d53dd3193cb66c27e47868b
Hery <hery@ittron.co.id>
2020-05-22 00:29:44 +0700

    add Base.php

---
+++ src/Base.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace \Cresenity\Core;
+
+use Symfony\Component\PropertyAccess\Exception\NoSuchIndexException;
+use Symfony\Component\PropertyAccess\Exception\NoSuchPropertyException;
+use Symfony\Component\PropertyAccess\PropertyAccess;
+
+class Base {
+
+    /**
+     * Get the class "basename" of the given object / class.
+     *
+     * @param  string|object  $class
+     * @return string
+     */
+    public static function classBasename($class) {
+        $class = is_object($class) ? get_class($class) : $class;
+
+        $basename = basename(str_replace('\\', '/', $class));
+        //$basename = carr::last(explode("_", $basename));
+        return $basename;
+    }
+
+}
